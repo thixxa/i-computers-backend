dbe615e60f381750c89a9519668e62406afdd080
Thisanda <thisandaprasanjana2002@gmail.com>
2025-11-22 11:46:20 +0530

    admin order page function created😊

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -110,3 +110,32 @@ export async function getOrders(req,res){
         res.json(orders);
     }
 }
+
+export async function updateOrderStatus(req,res){
+    if(!isAdmin(req)){
+        return res.status(403).json({
+            message : "Unauthorized access"
+        });
+        return;
+    }
+    try{
+        const orderId = req.params.orderId;
+        const status = req.body.status;
+        const notes = req.body.notes;
+
+        await Order.updateOne(
+            {orderId : orderId},
+            {status : status,
+            notes : notes
+        });
+        res.json({
+            message : "Order status updated successfully"
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            message : "Error updating order status",
+            error : error.message
+        });
+    }
+}
--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { createOrder } from "../controllers/orderController.js";
 import { getOrders } from "../controllers/orderController.js";
+import { updateOrderStatus } from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 orderRouter.post("/", createOrder);
 orderRouter.get("/", getOrders);
+orderRouter.put("/:orderId", updateOrderStatus);
 
 
 export default orderRouter;
